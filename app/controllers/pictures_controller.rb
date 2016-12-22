015e20856154b23f4f10ac2f8978f9bbba9eb6a8
Drew Haines <drew.dmdevco@gmail.com>
2016-12-21 19:46:04 -0800

    add some basic authentication

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,7 @@
 class PicturesController < ApplicationController
+  http_basic_authenticate_with name: "christmas_is_the_best", password: ENV['password']
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
 
   # GET /pictures
   # GET /pictures.json
